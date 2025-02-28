3b1a774c403e7ec233821c8664b3ac82da85ab12
arup.das1 <arup.das@anudip.org>
2025-02-28 17:26:13 +0530

    sds

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -235,8 +235,8 @@ class AttendanceController extends Controller
             date_default_timezone_set('Asia/Kolkata');
             
             foreach($request->details as $x){
-                return Response(['message' => 'inserted successfully vvv','status'=>1,'data11'=>sizeof($request->details)],200);
-                return Response(['message' => 'inserted successfully','status'=>1,'data11'=>$request->details],200);
+              
+                //return Response(['message' => 'inserted successfully','status'=>1,'data11'=>$request->details],200);
 
                 
 
@@ -253,7 +253,8 @@ class AttendanceController extends Controller
                     //     $attn_type='present';
                     // }
                     //     $attn_type='past
-                    $details = Attendance::where('atten_date', $x->attend_date)->where('user_id', $x->user_id)->get();
+                    $details1 = Attendance::where('atten_date', $x->attend_date)->where('user_id', $x->user_id)->get();
+                    return Response(['message' => 'inserted successfully vvv','status'=>1,'data11'=>$details1],200);
                     if($x->image!=''){
                         $s3_path="attendance/".trim($x->attend_date)."/";
                         $folderPath = "volume_blr1_01/".trim($x->attend_date)."/";
