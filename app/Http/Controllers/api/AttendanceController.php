c692b545658985dbdee5401ee9b63a1e070ac889
arup.das1 <arup.das@anudip.org>
2025-02-28 18:28:33 +0530

    sds

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -327,7 +327,7 @@ class AttendanceController extends Controller
                         $lastId=Attendance::create($postParameter)->id;
                         Photo::create(['user_id' => $x['user_id'],'attendance_id'=>$lastId,'punch_type'=>'I','photo_name'=>$input['file'],'lat'=>$x['lat'],'long'=>$x['long'],'place'=>'demo','punch_time'=>$x['punch_in'],'punch_date'=>$x['attend_date'],'member_code'=>trim($x['member_code'])]);
                     }else{
-                        dd($details1);
+                        //dd($details1);
                         if(sizeof($details1)>0){
 
                             Attendance::where('atten_date', $x['attend_date'])->where('user_id', $$details1[0]->user_id)->update(['punch_out'=>$x['punch_out'],'punch_out_lat'=>$x['lat'],'punch_out_long'=>$x['long'],'status'=>0,'punch_out_place'=>'demo']);
