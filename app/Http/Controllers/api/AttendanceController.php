13aa141bff2097e5e5285128ec5c17ae88b0d64d
arup.das1 <arup.das@anudip.org>
2025-02-28 18:00:55 +0530

    sds

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -256,7 +256,7 @@ class AttendanceController extends Controller
                     
                     $details1 = Attendance::where('atten_date', $x['attend_date'])->where('user_id', $x['user_id'])->get();
                     
-                    if($x->image!=''){
+                    if($x['image']!=''){
                         $s3_path="attendance/".trim($x['attend_date'])."/";
                         $folderPath = "volume_blr1_01/".trim($x['attend_date'])."/";
                         $base64Image = explode(";base64,", $x['image']);
