91f332c5a8a3aaa8bea3a706cb28cb60e7d62bd4
arup.das1 <arup.das@anudip.org>
2025-02-28 17:24:28 +0530

    sds

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -233,7 +233,7 @@ class AttendanceController extends Controller
             //return $request->details;
             //
             date_default_timezone_set('Asia/Kolkata');
-
+            return Response(['message' => 'inserted successfully','status'=>1,'data11'=>sizeof($request->details)],200);
             foreach($request->details as $x){
                 return Response(['message' => 'inserted successfully','status'=>1,'data11'=>$request->details],200);
 
