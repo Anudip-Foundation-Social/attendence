dd2936616fcc8a6f321c5ae5892c093470668a5b
arup.das1 <arup.das@anudip.org>
2025-03-03 13:47:48 +0530

    sds

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -437,7 +437,7 @@ class AttendanceController extends Controller
                                 'punch_type'=>"O",
                                 'photo'=>$input['file'],
                                 'batch_code'=>$x['batch_code'],
-                                'update_attn_status'=>$x['bulk_type']==1?1:0,
+                                'update_attn_status'=>0,
                             ]);
                             
                         }
