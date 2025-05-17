1d47999a9d5d00d81af2be8fe055deafe6312a6c
arup.das1 <arup.das@anudip.org>
2025-05-17 12:56:53 +0530

    asset api

--- app/Http/Controllers/api/TrainerController.php
+++ app/Http/Controllers/api/TrainerController.php
@@ -473,9 +473,9 @@ class TrainerController extends Controller
                     $details = Attendance::where('atten_date', $request->attend_date)->where('user_id', $user_id)->get();
 
                     Photo::create(['user_id' => $user_id,'attendance_id'=>$details[0]->id,'punch_type'=>'O','photo_name'=>$input['file'],'lat'=>$request->lat,'long'=>$request->long,'place'=>$request->location,'punch_time'=>$time,'punch_date'=>$request->attend_date,'member_code'=>trim($users[0]->member_code)]);
-                    dd('kj');
+                    
                     $mob_id=DB::connection('mysql_2')->table('attendance_app')->where('member_id',$member_id)->where('atten_date',$request->attend_date)->where('punch_type',"O")->get(['id']);
-
+                    dd($mob_id);
                     if(sizeof($mob_id)>0){
                         $mobile_id=$mob_id[0]->id;
                         DB::connection('mysql_2')->table('attendance_app')->updateOrInsert([
