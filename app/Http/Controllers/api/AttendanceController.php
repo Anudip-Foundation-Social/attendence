51d420b51ea4bcec7a10c9f828935713cac291a3
arup.das1 <arup.das@anudip.org>
2025-05-17 14:40:13 +0530

    asset api

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -860,7 +860,7 @@ class AttendanceController extends Controller
                                     'reason'          => $x['reason'],
                                     'bulk_type'       => 1,
                                     'created_by'      => $trainer_id,
-                                    'app_version'     => $x['app_version'],
+                                    'app_version'     => '1.0.1 (5)',
                                     'created_at'      => now(),
                                     'updated_at'      => now(),
                                 ]);
