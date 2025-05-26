a178a561c24e60ef56c7802674a88b4e000d90cb
arup.das1 <arup.das@anudip.org>
2025-05-26 13:36:28 +0530

    attn save

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -932,7 +932,6 @@ class AttendanceController extends Controller
                                 }
 
                                 
-                                  //dd($user_id);
                                 $details = Attendance::where('atten_date', $x['attend_date'])->where('user_id', $user_id)->get();
             
                                 Photo::create(['user_id' => $user_id,'attendance_id'=>$details[0]->id,'punch_type'=>'O','photo_name'=>$input['file'],'lat'=>$x['lat'],'long'=>$x['long'],'punch_time'=>$time,'punch_date'=>$x['attend_date'],'member_code'=>trim($users[0]->member_code)]);
