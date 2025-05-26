dab9da1161df78d2d17693a3a55093c7a5c79eff
arup.das1 <arup.das@anudip.org>
2025-05-26 17:39:00 +0530

    attendance data update

--- app/Http/Controllers/api/TrainerController.php
+++ app/Http/Controllers/api/TrainerController.php
@@ -411,7 +411,7 @@ class TrainerController extends Controller
                         'created_at'      => now(),
                         'updated_at'      => now(),
                     ]);
-                    dd($lastId);
+                    //dd($lastId);
 
                     Photo::create(['user_id' => $user_id,'attendance_id'=>$lastId,'punch_type'=>'I','photo_name'=>$input['file'],'lat'=>$request->lat,'long'=>$request->long,'place'=>$request->location,'punch_time'=>$time,'punch_date'=>$request->attend_date,'member_code'=>trim($users[0]->member_code)]);
                     
@@ -453,7 +453,7 @@ class TrainerController extends Controller
                         )
                     );
                     
-                    
+                   dd('dd'); 
                     
 
                 }else{
