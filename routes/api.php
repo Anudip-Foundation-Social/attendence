f9407120dfcb7cdd0f64b67191bd48d5980bda29
arup.das1 <arup.das@anudip.org>
2025-05-26 13:42:55 +0530

    huge placement work

--- routes/api.php
+++ routes/api.php
@@ -34,16 +34,16 @@ Route::get('fetchDataForCheckingRedis',[AttendanceController::class,'fetchDataFo
 Route::group(['middleware' => 'auth:sanctum'],function(){
     Route::get('user',[UserController::class,'userDetails']);
     Route::get('logout',[UserController::class,'logout']);
-    Route::post('store-attendance',[AttendanceController::class,'storeAttendance']);
-    Route::post('offlineSync',[AttendanceController::class,'offlineSync']);
-    Route::post('offlineSyncBulkPunchInOutAttendance',[AttendanceController::class,'offlineSyncBulkPunchInOutAttendance']);
+    Route::post('store-attendance-new',[AttendanceController::class,'storeAttendance']);
+    Route::post('offlineSync-new',[AttendanceController::class,'offlineSync']);
+    Route::post('offlineSyncBulkPunchInOutAttendance-new',[AttendanceController::class,'offlineSyncBulkPunchInOutAttendance']);
     Route::get('fetch-attendance/{user_id}/{cur_month}/{cur_year}',[AttendanceController::class,'fetchAttendance']);
     Route::get('fetch-attendance-based-on-currentdate/{user_id}/{cur_date?}',[AttendanceController::class,'fetchAttendanceBasedOnCurrentDate']);
 
     Route::get('fetch-center-for-trainer/{trainer_id}',[TrainerController::class,'fetchCenterForTrainer']);
     Route::get('fetch-batch-by-center/{center_id}',[TrainerController::class,'fetchBatchByCenter']);
     Route::get('fetch-student-by-batch/{batch_id}',[TrainerController::class,'fetchStudentByBatch']);
-    Route::post('store-bulk-punchin-out-attendance',[TrainerController::class,'storeBulkPunchInOutAttendance']);
+    Route::post('store-bulk-punchin-out-attendance-new',[TrainerController::class,'storeBulkPunchInOutAttendance']);
 
     Route::get('fetchAllDetailsForTrainer/{username}',[AttendanceController::class,'fetchAllDetailsForTrainer']);
 });
