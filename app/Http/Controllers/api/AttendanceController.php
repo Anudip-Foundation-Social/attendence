928cf3950cc5b2450d84a2d23b125d93177625b0
arup.das1 <arup.das@anudip.org>
2025-05-26 16:54:46 +0530

    attendance data update

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -569,7 +569,7 @@ class AttendanceController extends Controller
 
                             //if($details1[0]->status!=1 && $details1[0]->status!=3){
                             $sts=Attendance::where('atten_date', $request->attend_date)->where('user_id', $details1[0]->user_id)->get(['status','punch_out']);
-                            dd($sts);
+                            dd($sts,$details1[0]->user_id,$request->attend_date);
                             if(($sts[0]->status != 1 && $sts[0]->status != 3) ||
                             ($sts[0]->status == 1 && $sts[0]->punch_out == null)){
 
