452f6003835ceb71ef1946ad1d7c2a57a9ce26c8
arup.das1 <arup.das@anudip.org>
2025-05-27 19:04:56 +0530

    attendance data update

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -878,7 +878,7 @@ class AttendanceController extends Controller
 
                         $studenttime=Attendance::where('atten_date',$x['attend_date'])->where('user_id',$user_id)->get(['punch_in','punch_out','bulk_type','status']);
 
-                        if($studenttime[0]->bulk_type!=1 && $studenttime[0]->status!=1)
+                        if($studenttime[0]->bulk_type!=1 && $studenttime[0]->status!=1){
 
                             if($time<$studenttime[0]->punch_in){
                                 Attendance::where('atten_date', $x['attend_date'])->where('user_id', $user_id)->update(['punch_in'=>$time,'status'=>1,'punch_out_place'=>'']);
@@ -902,7 +902,7 @@ class AttendanceController extends Controller
 
                         $checkInTime=DB::connection('mysql_2')->table('attendance_app')->where('member_id',$member_id)->where('atten_date',$x['attend_date'])->where('punch_type','I')->value('punch_time');
 
-                        if($details[0]->bulk_type!=1 && $details[0]->status!=1)
+                        if($details[0]->bulk_type!=1 && $details[0]->status!=1){
 
                             if($checkInTime>$time){
                                 $checkInTime=DB::connection('mysql_2')->table('attendance_app')->where('member_id',$member_id)->where('atten_date',$x['attend_date'])->where('punch_type','I')->update(['punch_time' =>$time]);
