086bf9bf3f064dd3699c3583c8e19ece2524bd1d
arup.das1 <arup.das@anudip.org>
2025-05-27 16:51:31 +0530

    attendance data update

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -321,6 +321,23 @@ class AttendanceController extends Controller
                 //dd($checkInTime,$user_id);
                 if($checkInTime>$time){
                     $checkInTime=DB::connection('mysql_2')->table('attendance_app')->where('member_id',$member_id)->where('atten_date',$request->attend_date)->where('punch_type','I')->update(['punch_time' =>$time]);
+
+                    $mob_id=DB::connection('mysql_2')->table('attendance_app')->where('member_id',$member_id)->where('atten_date',$request->attend_date)->value('id');
+
+                        $insertGetBatchId = DB::connection('mysql_2')->table('attendance_records')->insertGetId(
+                            array(
+                                'source' => 'mobile_student',
+                                'mobile_app_id' => $mob_id,
+                                'member_id'=>$member_id,
+                                'member_type'=>'student',
+                                'punch_type'=>"I",
+                                'flag_value'=>1,
+                                'punch_time'=>$request->attend_date." ".$time,
+                                'onetime'=>1,
+                                'created_at'=>now(),
+                                'attd_month'=>'All',
+                            )
+                        );
                 }else{
                      
                     $checkOutTime=DB::connection('mysql_2')->table('attendance_app')->where('member_id',$member_id)->where('atten_date',$request->attend_date)->where('punch_type',"O")->value('punch_time');
@@ -359,7 +376,7 @@ class AttendanceController extends Controller
                                 'mobile_app_id' => $mob_id,
                                 'member_id'=>$member_id,
                                 'member_type'=>'student',
-                                'punch_type'=>"I",
+                                'punch_type'=>"O",
                                 'flag_value'=>1,
                                 'punch_time'=>$request->attend_date." ".$time,
                                 'onetime'=>1,
