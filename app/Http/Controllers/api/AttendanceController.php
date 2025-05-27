5460e530b4c76289bf98555726ef93bd881c1024
arup.das1 <arup.das@anudip.org>
2025-05-27 17:11:34 +0530

    attendance data update

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -792,7 +792,7 @@ class AttendanceController extends Controller
                             'punch_in_place'  => 'demo',
                             'reason'          => $x['reason'],
                             'bulk_type'       => 0,
-                            'app_version'     => $x['app_version'],
+                            'app_version'     => '1.0.1 (5)',
                             'created_at'      => now(),
                             'updated_at'      => now(),
                         ]);
