d04b7938cc8d83bc6be3efbae755f3eb5dc8bd00
arup.das1 <arup.das@anudip.org>
2025-05-27 16:55:47 +0530

    attendance data update

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -307,7 +307,7 @@ class AttendanceController extends Controller
 
                     if($time>$checkOutTime){
 
-                        Attendance::where('atten_date', $request->attend_date)->where('user_id', $user_id)->update(['punch_out'=>$time,'status'=>0]);
+                        Attendance::where('atten_date', $request->attend_date)->where('user_id', $user_id)->update(['punch_out'=>$time,'status'=>0,'punch_out_lat'=>$request->lat,'punch_out_long'=>$request->long]);
                     }
                 }
 
