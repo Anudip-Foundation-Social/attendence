1015ae4359f08aa5993b36e54a7512840df598c2
arup.das1 <arup.das@anudip.org>
2025-07-31 18:15:35 +0530

    add slot new

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -1596,18 +1596,18 @@ class AttendanceController extends Controller
                         //dd($student_list);
                         foreach($student_list as $member_id){
 
-                            $batchdata = DB::connection('mysql_2')->table('enrollments as a')
-                                ->join('batches as b', 'a.batch_id', '=', 'b.id')
-                                ->where('a.member_id', $member_id)
-                                ->orderByDesc('a.id')
-                                ->get(['a.batch_id', 'b.center_id', 'b.batch_code']);
-                            if(sizeof($batchdata)==0){
-                                $center_id=$x['center_id'];
-                                $batch_code=$x['batch_code'];
-                            }else{
-                                $center_id=$batchdata[0]->center_id;
-                                $batch_code=$batchdata[0]->batch_code;
-                            }  
+                            // $batchdata = DB::connection('mysql_2')->table('enrollments as a')
+                            //     ->join('batches as b', 'a.batch_id', '=', 'b.id')
+                            //     ->where('a.member_id', $member_id)
+                            //     ->orderByDesc('a.id')
+                            //     ->get(['a.batch_id', 'b.center_id', 'b.batch_code']);
+                            // if(sizeof($batchdata)==0){
+                            //     $center_id=$x['center_id'];
+                            //     $batch_code=$x['batch_code'];
+                            // }else{
+                            //     $center_id=$batchdata[0]->center_id;
+                            //     $batch_code=$batchdata[0]->batch_code;
+                            // }  
 
                             $incount=Attendance::where('atten_date',$x['attend_date'])->where('member_id',$member_id)->count();
                            // dd($incount);
@@ -1684,10 +1684,10 @@ class AttendanceController extends Controller
                                     'atten_type' => $atten_type,
                                     'member_type' => $member_type,
                                     'reason' => $x['reason'],
-                                    'center_id' => $center_id,
+                                    'center_id' => $x['center_id'],
                                     'punch_type' =>"I",
                                     'photo' => $input['file'],
-                                    'batch_code' => $batch_code,
+                                    'batch_code' => $x['batch_code'],
                                     'update_attn_status' => 1,
                                     'bulk_type' => 1,
                                     'approve_by' => $trainer_id,
