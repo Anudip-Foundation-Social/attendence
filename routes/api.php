9e105beec53379780ab6c01a6a44fcae7ce09c06
arup.das1 <arup.das@anudip.org>
2025-09-12 17:05:21 +0530

    out time add

--- routes/api.php
+++ routes/api.php
@@ -40,8 +40,8 @@ Route::group(['middleware' => 'auth:sanctum'],function(){
     Route::post('store-attendance-new-student',[AttendanceController::class,'storeAttendance']);
     Route::post('offlineSync-new-student',[AttendanceController::class,'offlineSync']);
     Route::post('offlineSyncBulkPunchInOutAttendance-new',[AttendanceController::class,'offlineSyncBulkPunchInOutAttendance']);
-    Route::get('fetch-attendance/{user_id}/{cur_month}/{cur_year}',[AttendanceController::class,'fetchAttendance']);
-    Route::get('fetch-attendance-based-on-currentdate/{user_id}/{cur_date?}',[AttendanceController::class,'fetchAttendanceBasedOnCurrentDate']);
+    Route::get('fetch-attendance-student/{user_id}/{cur_month}/{cur_year}',[AttendanceController::class,'fetchAttendance']);
+    Route::get('fetch-attendance-based-on-currentdate-student/{user_id}/{cur_date?}',[AttendanceController::class,'fetchAttendanceBasedOnCurrentDate']);
 
     Route::get('fetch-center-for-trainer/{trainer_id}',[TrainerController::class,'fetchCenterForTrainer']);
     Route::get('fetch-batch-by-center/{center_id}',[TrainerController::class,'fetchBatchByCenter']);
