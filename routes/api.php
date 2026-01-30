d3bcb55c91008521c39a6cf05c2c9aeed65a6efa
anudiparup <arup.das@anudip.org>
2026-01-30 14:21:27 +0530

    first

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,7 @@ Route::group(['middleware' => 'auth:sanctum'],function(){
     Route::post('offlineSync-new-student',[AttendanceController::class,'offlineSync']);
     Route::post('offlineSyncBulkPunchInOutAttendance-new',[AttendanceController::class,'offlineSyncBulkPunchInOutAttendance']);
 
-    Route::post('offlineSyncBulkPunchInOutAttendance_cron',[AttendanceController::class,'offlineSyncBulkPunchInOutAttendance_cron']);
+    Route::get('offlineSyncBulkPunchInOutAttendance_cron',[AttendanceController::class,'offlineSyncBulkPunchInOutAttendance_cron']);
 
     Route::get('fetch-attendance-student/{user_id}/{cur_month}/{cur_year}',[AttendanceController::class,'fetchAttendance']);
     Route::get('fetch-attendance-based-on-currentdate-student/{user_id}/{cur_date?}',[AttendanceController::class,'fetchAttendanceBasedOnCurrentDate']);
