5f54c2502fd2901877f9b980871740a79c26332f
anudiparup <arup.das@anudip.org>
2026-01-30 14:39:54 +0530

    first

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -2647,10 +2647,10 @@ class AttendanceController extends Controller
             if(sizeof($mail_content)){   
                
                 foreach($mail_content as $x){
-                    //dd($x);
+                    dd($x['image_name']);
                     
                         //dd(json_decode($a['studentList'], true));
-                        $student_list=json_decode($x['studentList'], true);
+                        //$student_list=json_decode($x['studentList'], true);
                 
                         date_default_timezone_set('Asia/Kolkata');
                         $time=$x['punch_time'];
