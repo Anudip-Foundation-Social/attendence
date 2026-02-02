67e4c965edfbc218afbbab180009a7d216d8e6fc
anudiparup <arup.das@anudip.org>
2026-02-02 14:14:58 +0530

    first

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -2623,15 +2623,15 @@ class AttendanceController extends Controller
             }
                 //dd($rows);
 
-                DB::connection('mysql_2')->table('mailer_service_details')->insert([
-                    'email_subject' => "CMIS - email attendance",
-                    'email_content'=> '<p><h3>'.json_encode($rows).'</h3></p>',
-                    'template_name'         => "auth.emails.mail_final_assessment_request",
-                    'email_receiver'       => 'arup.das@anudip.org',
-                    'email_cc_receiver'   => json_encode($email_cc),
-                    'email_attach_link'  => 'NA',
-                    'mail_topic'       => 'attn ("updateStudentEmail")',
-                    ]);
+                // DB::connection('mysql_2')->table('mailer_service_details')->insert([
+                //     'email_subject' => "CMIS - email attendance",
+                //     'email_content'=> '<p><h3>'.json_encode($rows).'</h3></p>',
+                //     'template_name'         => "auth.emails.mail_final_assessment_request",
+                //     'email_receiver'       => 'arup.das@anudip.org',
+                //     'email_cc_receiver'   => json_encode($email_cc),
+                //     'email_attach_link'  => 'NA',
+                //     'mail_topic'       => 'attn ("updateStudentEmail")',
+                //     ]);
 
                 DB::table('offline_student_sync_logs')->insertOrIgnore($rows);
             //}
