8f7e59d5ac2cd4bdacf77d139111906a2e6cd523
anudiparup <arup.das@anudip.org>
2026-02-02 12:42:42 +0530

    first

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -2552,7 +2552,7 @@ class AttendanceController extends Controller
                // dd($x);
 
                 $student_list = json_decode($x['studentList'] ?? '[]', true);
-                //dd($student_list,$x['studentList']);
+                dd($student_list,$x['studentList']);
 
                 
 
