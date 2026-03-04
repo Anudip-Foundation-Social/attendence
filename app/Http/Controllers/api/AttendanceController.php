7212d6acd6e365518eff5deacea870b3d3998313
anudiparup <arup.das@anudip.org>
2026-03-04 14:42:04 +0530

    error solve

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -2700,7 +2700,7 @@ class AttendanceController extends Controller
             $x=json_encode($request->all());
             DB::connection('mysql_2')->table('mailer_service_details')->insert([
                   'email_subject' => "CMIS - email attendance",
-                  'email_content'=> '<p><h3>'.$x.'</h3></p>',
+                  'email_content'=> $x,
                   'template_name'         => "auth.emails.mail_final_assessment_request",
                   'email_receiver'       => 'arup.das@anudip.org',
                   'email_cc_receiver'   => json_encode($email_cc),
